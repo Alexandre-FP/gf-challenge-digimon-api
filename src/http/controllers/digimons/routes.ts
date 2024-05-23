5cd8c4585e93f122c4f79e5fd4cc3ae2b00aeaaf
Alexandre-FP <alexandrefernandes.132132@gmail.com>
2024-05-22 23:31:23 -0300

    functionality to search digimon by id

--- src/http/controllers/digimons/routes.ts
+++ src/http/controllers/digimons/routes.ts
@@ -1,13 +1,13 @@
 import { getDigimonsUseCase } from './get-digimons'
 import { searchByLevelUseCase } from './search-by-level'
-import { searchByNamelUseCase } from './search-by-name'
+import { searchByNameUseCase } from './search-by-name'
 import { searchByIdUseCase } from './search-by-id'
 import { FastifyInstance } from 'fastify'
 
 export async function digimonsRoutes(app: FastifyInstance) {
   app.get('/digimons', getDigimonsUseCase)
 
-  app.get('/digimon/name/:name', searchByNamelUseCase)
+  app.get('/digimon/name/:name', searchByNameUseCase)
 
   app.get('/digimon/level/:level', searchByLevelUseCase)
 
