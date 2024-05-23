0e8f6cb88e53d8665505dda00fcdd13aba9a52b2
Alexandre-FP <alexandrefernandes.132132@gmail.com>
2024-05-22 23:52:51 -0300

    update file

--- src/repositories/digimon-repository.ts
+++ src/repositories/digimon-repository.ts
@@ -2,7 +2,7 @@ import { Digimon } from '@prisma/client'
 
 export interface DigimonsRepository {
   findById(id: string): Promise<Digimon | null>
-  allDigimon(): Promise<Digimon[] | null>
+  allDigimon(): Promise<Digimon[]>
   findByName(name: string): Promise<Digimon | null>
   findBylevel(level: string): Promise<Digimon | Digimon[] | null>
 }
